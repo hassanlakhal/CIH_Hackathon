e944ad435e272859e35b8d1bb12f59fe1ff2c293
5tirner <thatfor1337@gmail.com>
2026-04-18 11:08:42 +0100

    Change One

--- srcs/backend/core/users/views.py
+++ srcs/backend/core/users/views.py
@@ -6,12 +6,12 @@ from datetime import datetime
 def checkHealth(req):
     if req.method == "GET":
         now = datetime.now().strftime("%A, %d %B %Y - %H:%M:%S")
-    return HttpResponse(f"""
-        <div style="font-family: Arial; text-align: center; margin-top: 50px;">
-            <h1>Welcome to Cyclops</h1>
-            <p style="font-size: 18px; color: #555;">
-                Current time: {now}
-            </p>
-        </div>
-    """)
+        return HttpResponse(f"""
+            <div style="font-family: Arial; text-align: center; margin-top: 50px;">
+                <h1>Welcome to Cyclops</h1>
+                <p style="font-size: 18px; color: #555;">
+                    Current time: {now}
+                </p>
+            </div>
+        """)
     
